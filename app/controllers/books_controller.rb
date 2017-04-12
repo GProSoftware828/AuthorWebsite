8768d151147624d28ad14ce8123466ad2c4f27f6
GproSoftware828 <JetSendProductions@Gmail.com>
2017-04-12 14:23:30 -0400

    paths for simple form webpages

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,11 @@
 class BooksController < ApplicationController
+	before_action :find_book, only: [:show, :edit, :update, :destroy]
 	
 	def index
+	  @books = Book.all.order("created_at DESC")
+	end
+	
+	def show
 	end
 	
 	def new
@@ -9,12 +14,39 @@ class BooksController < ApplicationController
 	
 	def create
 	  @book = Book.new(book_params)
+	  
+	  if @book.save
+	    redirect_to root_path
+	  else
+	    render 'new'
+	  end
+	end
+	
+	def edit
+	  
+	end
+	
+	def update
+	  if @book.update(book_params)
+	    redirect_to book_path(@book)
+	  else
+	    render 'edit'
+	  end
+	end
+	
+	def destroy
+	  @book.destroy
+	  redirect_to root_path
 	end
 	
 	private
 	
 	def book_params
 	  params.require(:book).permit(:title, :description, :subtitle)
+	end
+	
+	def find_book
+	  @book = Book.find(params[:id])
 	end
 	
 end
